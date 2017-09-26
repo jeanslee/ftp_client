439938ed8adba8a6fe559389187759bf546d61c9
user name <email-for-github>
2017-09-26 16:38:52 +0800

    the First commit---FTP client

---
+++ sh.sh
@@ -0,0 +1,11 @@
+#   …   or create a new repository on the command line
+
+
+echo "# ftp_client_prj " >> README.md
+git init
+git add .
+#git commit -m "second commit---Openwrt SDK"
+git commit -m "the First commit---FTP client"
+git remote add origin https://github.com/jeanslee/ftp_client.git
+git push -u origin master
+git push
